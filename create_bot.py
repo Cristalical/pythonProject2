1066c6a7e013aa1bbc8b390e46332ef012ee3071
Шурик <ak3146753@gmail.com>
2023-08-05 20:57:53 +0700

    5

--- create_bot.py
+++ create_bot.py
@@ -1,4 +1,4 @@
 from aiogram import Dispatcher, Bot
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-bot = Bot("6420484725:AAHU-UQ2oylCgjR_wTiiTsseB-lYQS6g1js", parse_mode="html") #
+bot = Bot("6420484725:AAHU-UQ2oylCgjR_wTiiTsseB-lYQS6g1js", parse_mode="html")
 dp = Dispatcher(bot, storage=MemoryStorage())
--- keyboard.py
+++ keyboard.py
@@ -1,6 +1,9 @@
 from aiogram import types
 async def keyboard(message: types.Message):
+
     kb = types.InlineKeyboardMarkup()
-    kb.add(types.InlineKeyboardButton(text="🟣 Wildberries", callback_data="Wildberries"))
-    kb.add(types.InlineKeyboardButton(text="🔵 Ozon", callback_data="Ozon"))
+    kb.add(types.InlineKeyboardButton(
+    text="🟣 Wildberries",
+    switch_inline_query_current_chat="Wildberries "))
+    kb.add(types.InlineKeyboardButton(text="🔵 Ozon", switch_inline_query_current_chat="Ozon "))
     await message.answer("Нажмите для выбора парсинга сайта.", reply_markup=kb)
