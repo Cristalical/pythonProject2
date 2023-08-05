cadb431e6a7fea7d05bb80752f3c8909bd21cd06
Шурик <ak3146753@gmail.com>
2023-08-05 19:33:53 +0700

    5

--- TGbot.py
+++ TGbot.py
@@ -1,6 +1,6 @@
 #Сам ботик
 import logging
-from aiogram import executor
+from aiogram import executor, types
 from create_bot import dp
 from WB_parser import cmd_test1
 from OZ_parser import cmd_test2
@@ -12,7 +12,29 @@ logging.basicConfig(level=logging.INFO)
 
 dp.register_message_handler(cmd_test2, commands="OZ")
 dp.register_message_handler(cmd_test1, commands="WB")
+async def cmd_start(message: types.Message):
+    kb = [
+        [
+            types.KeyboardButton(text="С пюрешкой"),
+            types.KeyboardButton(text="Без пюрешки")
+        ],
+    ]
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True,
+        input_field_placeholder="Выберите способ подачи"
+    )
+    await message.answer("Как подавать котлеты?", reply_markup=keyboard)
 
+    from aiogram.dispatcher.filters import Text
+    @dp.message_handler(Text(equals="С пюрешкой"))
+    async def with_puree(message: types.Message):
+        await message.reply("Отличный выбор!")
+
+    @dp.message_handler(lambda message: message.text == "Без пюрешки")
+    async def without_puree(message: types.Message):
+        await message.reply("Так невкусно!")
+dp.register_message_handler(cmd_start, commands="STR")
 
 if __name__ == "__main__":
     # Запуск бота
--- WB_parser.py
+++ WB_parser.py
@@ -2,3 +2,4 @@
 from aiogram import types
 async def cmd_test1(message: types.Message):
     await message.reply("WB_parse")
+    await message.reply(message.from_user.username)
