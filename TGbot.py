4d6f2015114167603ffe8b2e3202298e1d12edcc
Mega Salos <megasalos1@gmail.com>
2023-08-05 16:39:03 +0300

    update

--- TGbot.py
+++ TGbot.py
@@ -2,16 +2,14 @@
 import logging
 from aiogram import executor
 from create_bot import dp
+from keyboard import keyboard
 from WB_parser import cmd_test1 #ИСПОЛЬЗУЕТСЯ!!!
 from OZ_parser import cmd_test2 #ИСПОЛЬЗУЕТСЯ!!!
-from keyboard import keyboard
 
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
 
-
 dp.register_message_handler(keyboard, commands="start")
-
 
 if __name__ == "__main__":
     # Запуск бота
