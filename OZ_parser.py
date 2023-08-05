3ddfbd1cec02da9aea3079f524e68ea7b9ddc753
Mega Salos <megasalos1@gmail.com>
2023-08-05 15:04:56 +0300

    update

--- OZ_parser.py
+++ OZ_parser.py
@@ -2,3 +2,4 @@
 from aiogram import types
 async def cmd_test2(message: types.Message):
     await message.reply("OZ_parse")
+    await message.answer(f'<a href = "tg://user?id={message.from_id}" > Святозар </a> ', parse_mode='HTML')
