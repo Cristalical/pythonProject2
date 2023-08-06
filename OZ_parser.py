71e28177db9b5f00c7d670565f645e6c0d25d30c
Шурик <ak3146753@gmail.com>
2023-08-06 21:11:53 +0700

    9

--- OZ_parser.py
+++ OZ_parser.py
@@ -1,22 +1,29 @@
 #Парсер 2
 from bs4 import BeautifulSoup
-from soup_manager import soupManager
+from selenium import webdriver
 from aiogram import types
 from create_bot import dp
 @dp.message_handler(text="Ozon")
 async def OZ_parse(message: types.Message, url):
     await message.answer("Начинается парсинг по запросу ⬇️\n"+url)
-    def parse(url):
-        headers = {
-            'Accept': "application/json",
-            'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 OPR/101.0.0.0 (Edition Yx 05)"
-        }
-        soup = soupManager(url, headers)
-        if type(soup) is BeautifulSoup:
-            #next_url = url.func()
-            return next_url
-        else:
-            print(f"Error: soup hasn't type BeautifulSoup. Find type "+type(soup))
-            return False
-    while url:
-        url = parse(url)
+    driver = webdriver.Chrome()
+    driver.get(url)
+
+    soup = BeautifulSoup(driver.page_source)
+    url_new = soup.find('a', class_='a2-a4').get('href')
+    print(url_new)
+
+
+
+
+
+
+
+
+
+
+    headers = {
+        'Accept': "application/json",
+        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 OPR/101.0.0.0 (Edition Yx 05)"
+    }
+
--- soup_manager.py
+++ soup_manager.py
@@ -1,10 +1,13 @@
-import requests
-from bs4 import BeautifulSoup
-def soupManager(url, headers):
-    res = requests.get(url, headers=headers)
-    if res:
-        res.encoding = 'windows-1251'
-        return BeautifulSoup(res.text, 'lxml')
-    else:
-        print(f"Error: {res.status_code}")
-        return False
+
+
+
+# import requests
+# from bs4 import BeautifulSoup
+# def soupManager(url, headers):
+#     res = requests.get(url, headers=headers)
+#     if res:
+#         res.encoding = 'windows-1251'
+#         return BeautifulSoup(res.text, 'lxml')
+#     else:
+#         print(f"Error: {res.status_code}")
+#         return False
