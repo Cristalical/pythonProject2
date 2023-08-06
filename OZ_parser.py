f6fd31f36dc3bdee5866e46ff7cef9fb8a091ef1
Шурик <ak3146753@gmail.com>
2023-08-06 16:55:10 +0700

    9

--- OZ_parser.py
+++ OZ_parser.py
@@ -6,14 +6,17 @@ from create_bot import dp
 @dp.message_handler(text="Ozon")
 async def OZ_parse(message: types.Message, url):
     await message.answer("Начинается парсинг по запросу ⬇️\n"+url)
-    headers = {
-        'Accept': "application/json",
-        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 OPR/101.0.0.0 (Edition Yx 05)"
-    }
-    soup = soupManager(url, headers)
-    if type(soup) is BeautifulSoup:
-        pass
-    else:
-        print(f"Error: soup hasn't type BeautifulSoup. Find type " + type(soup))
-        return False
-
+    def parse(url):
+        headers = {
+            'Accept': "application/json",
+            'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 OPR/101.0.0.0 (Edition Yx 05)"
+        }
+        soup = soupManager(url, headers)
+        if type(soup) is BeautifulSoup:
+            #next_url = url.func()
+            return next_url
+        else:
+            print(f"Error: soup hasn't type BeautifulSoup. Find type "+type(soup))
+            return False
+    while url:
+        url = parse(url)
