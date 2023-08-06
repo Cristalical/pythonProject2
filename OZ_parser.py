84fe2f8a94508224270addf55894887fc95c9647
Mega Salos <megasalos1@gmail.com>
2023-08-06 12:32:38 +0300

    update

--- OZ_parser.py
+++ OZ_parser.py
@@ -1,9 +1,11 @@
 #Парсер 2
+from bs4 import BeautifulSoup
+from soup_manager import soupManager
 from aiogram import types
 from create_bot import dp
 @dp.message_handler(text="Ozon")
 async def OZ_parse(message: types.Message, url):
-    await message.answer("Начинается парсинг по запросу "+url)
+    await message.answer("Начинается парсинг по запросу ⬇️\n"+url)
     headers = {
         'Accept': "*/*",
         'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 OPR/100.0.0.0 (Edition Yx GX)"
