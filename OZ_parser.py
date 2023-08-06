f95f799fc25a6e10b74c376bf82a81bb031d10ab
Mega Salos <megasalos1@gmail.com>
2023-08-06 12:51:01 +0300

    update

--- OZ_parser.py
+++ OZ_parser.py
@@ -7,10 +7,13 @@ from create_bot import dp
 async def OZ_parse(message: types.Message, url):
     await message.answer("Начинается парсинг по запросу ⬇️\n"+url)
     headers = {
-        'Accept': "*/*",
-        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 OPR/100.0.0.0 (Edition Yx GX)"
+        'Accept': "application/json",
+        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 OPR/101.0.0.0 (Edition Yx 05)"
     }
     soup = soupManager(url, headers)
     if type(soup) is BeautifulSoup:
         pass
+    else:
+        print(f"Error: soup hasn't type BeautifulSoup. Find type " + type(soup))
+        return False
 
