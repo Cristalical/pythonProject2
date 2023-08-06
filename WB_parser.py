cf68a50ab4d5bbcd527a058daa99ab6334e28be1
Mega Salos <megasalos1@gmail.com>
2023-08-06 16:21:47 +0300

    update

--- WB_parser.py
+++ WB_parser.py
@@ -1,25 +1,17 @@
 #Парсер 1
-from bs4 import BeautifulSoup
-from soup_manager import soupManager
 from aiogram import types
 from create_bot import dp
+import requests
 @dp.message_handler(text="Wildberries")
 async def WB_parse(message: types.Message, url):
     await message.answer("Начинается парсинг по запросу ⬇️\n"+url)
-    def parse(url):
-        headers = {
-            'Accept': "*/*",
-            'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 OPR/100.0.0.0 (Edition Yx GX)"
-        }
-        soup = soupManager(url, headers)
-        if type(soup) is BeautifulSoup:
-            #next_url = url.func()
-            return next_url
-        else:
-            print(f"Error: soup hasn't type BeautifulSoup. Find type "+type(soup))
-            return False
-    while url:
-        url = parse(url)
+    #headers = {
+    #    'Accept': "*/*",
+    #    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.188"
+    #}
+    #res = requests.get(url, headers=headers)
+    #with open("txt.json", 'w', encoding="utf-8") as f:
+    #    f.write(res.json())
 
 
 
