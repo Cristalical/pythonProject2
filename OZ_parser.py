5b5da183e794e5235bdb54017ca83142e3c8d0cd
Шурик <ak3146753@gmail.com>
2023-08-07 21:09:28 +0700

    up

--- OZ_parser.py
+++ OZ_parser.py
@@ -6,28 +6,43 @@ from Saving import save
 @dp.message_handler(text="Ozon")
 async def OZ_parse(message: types.Message, url):
     await message.answer("Начинается парсинг по запросу ⬇️\n"+url)
-    url = url
+    # Переменная для сохранения данных в таблицу, используется для создания строк и
+    # нумерации полученных данных в файле Saving.py
     i = 2
+    # Основная функция для парсинга данных
     def parsing(url, i):
         soup = soupManager(url)
-        url_new = 'https://www.ozon.ru' + soup.find('div', class_='tw2 a2-a').find('a', class_='a2-a4').get('href')
-        cells = soup.find_all('div', class_='i9j ik')
 
+        # Исключение, созданное для окончания работы парсера с помощью выхода из цикла
+        try:
+        # Ссылка на следующую страницу с товарами
+            url_new = 'https://www.ozon.ru' + soup.find('a', class_='a2-a4').get('href')
+        except:
+            url_new = False
+        # Данные со всех ячеек товаров, сохранённые в список
+        cells = soup.find_all('div', class_='i9j ik')
+        # Цикл для перебора каждой ячейки из списка
         for cell in cells:
+            # Получение имени товара
             name = cell.find('span', class_='tsBody500Medium').text
+            # Получение цены товара
             price = cell.find('span', class_='c3-a1 tsHeadline500Medium c3-b9').text
+            # Исключение для обхода ошибки, которая появляется из-за отсутствия рейтинга у некоторых товаров
             try:
+                # Получение рейтинга товара
                 rating = cell.find('svg', class_='du4').findNext('span').text
             except:
                 rating = "Нет оценки"
+            # Получение ссылки на товар
             link = 'https://www.ozon.ru' + cell.find('a', class_='tile-hover-target yh3 h4y').get('href')
-            #save(name, price, rating, link, i)
-
+            # Вызов функции для сохранения данных в Excel
+            save(name, price, rating, link, i)
+        # Функция возвращает URL следующей страницы с товарами
         return url_new
     while url:
+        # Вызов функции парсинга и присвоение переменной нового URL
         url = parsing(url, i)
+        # Увеличение значения переменной для занесения данных в следующую строку в Excel
         i += 1
-        await message.answer(url)
-
-
-
+        # Вывод нового URL в чат телеграмм для видимости работы парсера
+        await message.answer("ХОП")
--- Saving.py
+++ Saving.py
@@ -1,32 +1,37 @@
 import openpyxl as op
 def save(name, price, rating, link, i):
-    #имя файла
-    filename = 'OZ.xlsx'
-    #создание Workbook
-    wb = op.Workbook()
+    # Имя файла
+    filename = 'result_OZ.xlsx'
+    # Открытие Excel и создание Workbook
+    wb = op.load_workbook(filename)
     sheet = wb.active
-    #Вводим таблицы
+    # Вводим таблицы
     c = sheet.cell(row = 1, column = 1)
     c1 = sheet.cell(row = 1, column = 2)
     c2 = sheet.cell(row = 1, column = 3)
     c3 = sheet.cell(row = 1, column = 4)
     c4 = sheet.cell(row = 1, column = 5)
-    #Вводим данные таблицы
+    # Ввод названий столбцов
     c.value = "№"
     c1.value = "Название"
     c2.value = "Цена"
     c3.value = "Рейтинг"
     c4.value = "Ссылка"
-    #Сохраняем файл
+    # Занесение данных в строки
     c = sheet.cell(row=i, column=1)
     c1 = sheet.cell(row=i, column=2)
     c2 = sheet.cell(row=i, column=3)
     c3 = sheet.cell(row=i, column=4)
     c4 = sheet.cell(row=i, column=5)
+    # Занесение номера
     c.value = i-1
+    # Занесение имени
     c1.value = name
+    # Занесение цены
     c2.value = price
+    # Занесение рейтинга
     c3.value = rating
+    # Занесение ссылки
     c4.value = link
-    i += 1
-    wb.save(filename)
+    # Сохранение данных в таблицу Excel
+    wb.save()
