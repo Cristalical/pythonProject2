20f639bac8c816c153cb1741b019544dd5544fad
Шурик <ak3146753@gmail.com>
2023-08-09 00:34:24 +0700

    up

--- WB_parser.py
+++ WB_parser.py
@@ -6,14 +6,9 @@ from create_bot import dp
 async def WB_parse(message: types.Message, url):
     await message.answer("Начинается парсинг по запросу ⬇️\n"+url)
     soup = soupManager(url)
-    url_new = soup.find('a', class_='pagination-next pagination__next j-next-page')
-    with open('txt.txt', "w", encoding='utf-8') as f:
-        f.write(str(soup))
+    url_new = soup.find('a', class_='pagination')
     print(url_new)
-    try:
-        await message.answer(url_new)
-    except:
-        print('try')
+    await message.answer(url_new)
 
 
 
